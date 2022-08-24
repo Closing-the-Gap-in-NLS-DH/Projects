65aa5ce61ea73a6ff04dee6ecd2f6df7764953db
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-08-24 11:55:25 +0200

    Delete vestigial file

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/Closing-The-Gap-In-Non-Latin-Script-Data/"
-      : "/",
-});
