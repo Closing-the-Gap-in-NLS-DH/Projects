79c39428e98a57c05aa7d6d9f81e0ba1a58df2b2
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-05-08 10:24:02 +0200

    Updates

---
+++ _app/immutable/nodes/3.fa278cd1.js
@@ -0,0 +1,47 @@
+import{S as Mt,i as Pt,s as qt,a as c,k as i,q as o,Q as Vt,h as a,c as d,l as r,m as n,r as l,n as e,b as Jt,D as t,E as _t}from"../chunks/index.05b06d0d.js";function Tt(Ct){let G,k,u,H,q,V,f,h,T,p,R,Q,g,K,W,x,X,Y,Z,I,$,tt,N,et,at,v,D,it,w,rt,nt,ot,j,lt,st,s,z,b,ht,ct,L,dt,y,ut,ft,vt,B,_,mt,pt,J,gt,wt,C,E,bt;return{c(){G=c(),k=i("div"),u=i("div"),H=i("h2"),q=o("About Us"),V=c(),f=i("div"),h=i("p"),T=o(`This database was created as part of the project
+				`),p=i("a"),R=o("Closing the Gap in Non-Latin-Script Data"),Q=o(`
+				under the auspices of the
+				`),g=i("a"),K=o("Berlin University Alliance"),W=o(`
+				and the
+				`),x=i("a"),X=o("Freie Universität Berlin"),Y=o(`, which aimed to improve the agency and visibility of NLS-related research in
+				the Digital Humanities in Germany. One part of the project was to get an idea of
+				the state of the field of “Digitale Arabistik” in Germany, mainly in terms of
+				completed, ongoing, and future research projects, but also in terms of available
+				infrastructure and relevant initiatives in the field.`),Z=c(),I=i("p"),$=o(`As we began gathering data on digital projects dealing with Arabic or similar
+				languages, we thought about how to provide this data in a way that commits to
+				OpenScience principles. So we chose a public Git repository as our main data
+				store, offering the data as JSON in a way that should be as straightforward as
+				possible. Everyone who is interested should be able to contribute without having
+				to deal with too much of a technology stack.`),tt=c(),N=i("p"),et=o(`For those who are not able or willing to work with JSON, we developed a
+				graphical user interface—this website—which offers not only a list of all
+				projects for which we have collected data, but also small visualizations (on
+				which we continue to iterate). For an easier way to provide data than writing
+				plain JSON, we offer a web form that can generate a file conforming to our
+				schema. All of these features will be improved over time, depending on the needs
+				and demands of the community.`),at=c(),v=i("div"),D=i("p"),it=o(`If you want to contribute in any way, to work on a localization of this service
+				or to report bugs or feature requests, feel free to contact us via
+				`),w=i("a"),rt=o("GitHub"),nt=o("."),ot=c(),j=i("p"),lt=o("Other initiatives and institutions with which we are involved include:"),st=c(),s=i("ul"),z=i("li"),b=i("a"),ht=o("Multilingual DH Lab @ Freie Universität Berlin"),ct=c(),L=i("li"),dt=o(`Staats- und Universitätsbibliothek Hamburg Carl von Ossietzky (Dr. Jonas
+					Müller-Laackman, Referent `),y=i("a"),ut=o("Digitale Forschungsdienste"),ft=o(")"),vt=c(),B=i("li"),_=i("a"),mt=o("DHd AG Multilingual DH"),pt=c(),J=i("li"),gt=o("Decolonization initiatives @ Freie Universität Berlin"),wt=c(),C=i("li"),E=i("a"),bt=o("Seminar for Semitic and Arabic Studies"),this.h()},l(S){Vt("svelte-1hp5igj",document.head).forEach(a),G=d(S),k=r(S,"DIV",{class:!0});var Et=n(k);u=r(Et,"DIV",{class:!0});var O=n(u);H=r(O,"H2",{class:!0});var Dt=n(H);q=l(Dt,"About Us"),Dt.forEach(a),V=d(O),f=r(O,"DIV",{class:!0});var U=n(f);h=r(U,"P",{});var A=n(h);T=l(A,`This database was created as part of the project
+				`),p=r(A,"A",{class:!0,href:!0,target:!0,rel:!0});var St=n(p);R=l(St,"Closing the Gap in Non-Latin-Script Data"),St.forEach(a),Q=l(A,`
+				under the auspices of the
+				`),g=r(A,"A",{class:!0,href:!0,target:!0,rel:!0});var At=n(g);K=l(At,"Berlin University Alliance"),At.forEach(a),W=l(A,`
+				and the
+				`),x=r(A,"A",{class:!0,href:!0});var kt=n(x);X=l(kt,"Freie Universität Berlin"),kt.forEach(a),Y=l(A,`, which aimed to improve the agency and visibility of NLS-related research in
+				the Digital Humanities in Germany. One part of the project was to get an idea of
+				the state of the field of “Digitale Arabistik” in Germany, mainly in terms of
+				completed, ongoing, and future research projects, but also in terms of available
+				infrastructure and relevant initiatives in the field.`),A.forEach(a),Z=d(U),I=r(U,"P",{});var xt=n(I);$=l(xt,`As we began gathering data on digital projects dealing with Arabic or similar
+				languages, we thought about how to provide this data in a way that commits to
+				OpenScience principles. So we chose a public Git repository as our main data
+				store, offering the data as JSON in a way that should be as straightforward as
+				possible. Everyone who is interested should be able to contribute without having
+				to deal with too much of a technology stack.`),xt.forEach(a),tt=d(U),N=r(U,"P",{});var Lt=n(N);et=l(Lt,`For those who are not able or willing to work with JSON, we developed a
+				graphical user interface—this website—which offers not only a list of all
+				projects for which we have collected data, but also small visualizations (on
+				which we continue to iterate). For an easier way to provide data than writing
+				plain JSON, we offer a web form that can generate a file conforming to our
+				schema. All of these features will be improved over time, depending on the needs
+				and demands of the community.`),Lt.forEach(a),U.forEach(a),at=d(O),v=r(O,"DIV",{class:!0});var F=n(v);D=r(F,"P",{class:!0});var M=n(D);it=l(M,`If you want to contribute in any way, to work on a localization of this service
+				or to report bugs or feature requests, feel free to contact us via
+				`),w=r(M,"A",{class:!0,href:!0,target:!0,rel:!0});var Gt=n(w);rt=l(Gt,"GitHub"),Gt.forEach(a),nt=l(M,"."),M.forEach(a),ot=d(F),j=r(F,"P",{});var Ht=n(j);lt=l(Ht,"Other initiatives and institutions with which we are involved include:"),Ht.forEach(a),st=d(F),s=r(F,"UL",{});var m=n(s);z=r(m,"LI",{});var Ot=n(z);b=r(Ot,"A",{class:!0,href:!0,target:!0,rel:!0});var Ut=n(b);ht=l(Ut,"Multilingual DH Lab @ Freie Universität Berlin"),Ut.forEach(a),Ot.forEach(a),ct=d(m),L=r(m,"LI",{});var P=n(L);dt=l(P,`Staats- und Universitätsbibliothek Hamburg Carl von Ossietzky (Dr. Jonas
+					Müller-Laackman, Referent `),y=r(P,"A",{class:!0,href:!0,target:!0,rel:!0});var Ft=n(y);ut=l(Ft,"Digitale Forschungsdienste"),Ft.forEach(a),ft=l(P,")"),P.forEach(a),vt=d(m),B=r(m,"LI",{});var It=n(B);_=r(It,"A",{class:!0,href:!0,target:!0,rel:!0});var Nt=n(_);mt=l(Nt,"DHd AG Multilingual DH"),Nt.forEach(a),It.forEach(a),pt=d(m),J=r(m,"LI",{});var jt=n(J);gt=l(jt,"Decolonization initiatives @ Freie Universität Berlin"),jt.forEach(a),wt=d(m),C=r(m,"LI",{});var zt=n(C);E=r(zt,"A",{class:!0,href:!0,target:!0,rel:!0});var Bt=n(E);bt=l(Bt,"Seminar for Semitic and Arabic Studies"),Bt.forEach(a),zt.forEach(a),m.forEach(a),F.forEach(a),O.forEach(a),Et.forEach(a),this.h()},h(){document.title="Closing the Gap in Non-Latin-Script Data – About",e(H,"class","text-center text-2xl"),e(p,"class","font-medium hover:underline"),e(p,"href","https://www.berlin-university-alliance.de/commitments/research-quality/forschung/ongoing/closing-the-gap/index.html"),e(p,"target","_blank"),e(p,"rel","noreferrer"),e(g,"class","font-medium hover:underline"),e(g,"href","https://www.berlin-university-alliance.de/index.html"),e(g,"target","_blank"),e(g,"rel","noreferrer"),e(x,"class","font-medium hover:underline"),e(x,"href","https://www.fu-berlin.de/"),e(f,"class","space-y-4"),e(w,"class","font-medium hover:underline"),e(w,"href","https://github.com/M-L-D-H/Closing-The-Gap-In-Non-Latin-Script-Data"),e(w,"target","_blank"),e(w,"rel","noreferrer"),e(D,"class","text-xl"),e(b,"class","font-medium hover:underline"),e(b,"href","https://www.ada.fu-berlin.de/ada-labs/multilingual-dh/index.html"),e(b,"target","_blank"),e(b,"rel","noreferrer"),e(y,"class","font-medium hover:underline"),e(y,"href","https://www.sub.uni-hamburg.de/service/digitale-forschungsdienste.html"),e(y,"target","_blank"),e(y,"rel","noreferrer"),e(_,"class","font-medium hover:underline"),e(_,"href","https://dig-hum.de/ag-multilingual-dh"),e(_,"target","_blank"),e(_,"rel","noreferrer"),e(E,"class","font-medium hover:underline"),e(E,"href","https://www.geschkult.fu-berlin.de/e/semiarab/arabistik/index.html"),e(E,"target","_blank"),e(E,"rel","noreferrer"),e(v,"class","space-y-4 text-center"),e(u,"class","space-y-4 rounded-lg bg-[#b8b08d] p-4 text-lg"),e(k,"class","mx-auto max-w-6xl px-4")},m(S,yt){Jt(S,G,yt),Jt(S,k,yt),t(k,u),t(u,H),t(H,q),t(u,V),t(u,f),t(f,h),t(h,T),t(h,p),t(p,R),t(h,Q),t(h,g),t(g,K),t(h,W),t(h,x),t(x,X),t(h,Y),t(f,Z),t(f,I),t(I,$),t(f,tt),t(f,N),t(N,et),t(u,at),t(u,v),t(v,D),t(D,it),t(D,w),t(w,rt),t(D,nt),t(v,ot),t(v,j),t(j,lt),t(v,st),t(v,s),t(s,z),t(z,b),t(b,ht),t(s,ct),t(s,L),t(L,dt),t(L,y),t(y,ut),t(L,ft),t(s,vt),t(s,B),t(B,_),t(_,mt),t(s,pt),t(s,J),t(J,gt),t(s,wt),t(s,C),t(C,E),t(E,bt)},p:_t,i:_t,o:_t,d(S){S&&a(G),S&&a(k)}}}class Qt extends Mt{constructor(G){super(),Pt(this,G,null,Tt,qt,{})}}export{Qt as component};
