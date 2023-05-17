22466dab78a0962bfa802ec663cf899d49030f2b
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-05-17 20:28:48 +0200

    Updates

---
+++ _app/immutable/chunks/stores.6989ab7d.js
@@ -0,0 +1,1 @@
+import"./index.f12d4e92.js";import{s as e}from"./singletons.52af8e59.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
--- _app/immutable/chunks/stores.7dff74cd.js
+++
@@ -1,1 +0,0 @@
-import"./index.f12d4e92.js";import{s as e}from"./singletons.e5d9eb6d.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
