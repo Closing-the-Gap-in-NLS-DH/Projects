29bfcdcdb48bd2dc806229074b61876862229ed0
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-06-17 15:45:30 +0200

    Add some tests

--- SCHEMATA/schema_tests.ts
+++ SCHEMATA/schema_tests.ts
@@ -1,4 +1,5 @@
-import { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.192.0/testing/asserts.ts";
+import { v4 } from "https://deno.land/std@0.192.0/uuid/mod.ts";
 import { keywords, projectSchema } from "./project_schema.ts";
 
 Deno.test("ensure no duplicate keywords", () => {
@@ -31,5 +32,47 @@ for (const [id, details] of projects) {
     const projectFile = Deno.readTextFileSync(path);
     const projectObj = JSON.parse(projectFile);
     projectSchema.parse(projectObj);
+
+    const uuid = projectObj.record_metadata.uuid;
+
+    // Validate UUID; Zod also does this, but not as strictly
+    assertEquals(v4.validate(uuid), true);
+
+    // Ensure filename matches internal UUID
+    assertEquals(uuid, id);
   });
+}
+
+// Get a sorted list of just IDs from PROJECTS.json
+const projectIds = projects.map(([id]) => id);
+projectIds.sort();
+
+Deno.test("ensure no duplicate IDs in projects list", () => {
+  const projectIdSet = new Set(projectIds);
+  assertEquals(projectIdSet.size, projectIds.length);
+});
+
+Deno.test("ensure filenames match listed IDs", () => {
+  const filenameIds = getIdsFromFilenames("PROJECTS");
+  filenameIds.sort();
+  assertEquals(filenameIds, projectIds);
+});
+
+//
+// Helper functions
+//
+
+function getIdsFromFilenames(basePath: string): string[] {
+  const ids = [];
+
+  for (const dirEntry of Deno.readDirSync(basePath)) {
+    if (dirEntry.isDirectory) {
+      const subPath = `${basePath}/${dirEntry.name}`;
+      ids.push(...getIdsFromFilenames(subPath));
+    } else {
+      ids.push(dirEntry.name.slice(0, -5));
+    }
+  }
+
+  return ids;
 }
