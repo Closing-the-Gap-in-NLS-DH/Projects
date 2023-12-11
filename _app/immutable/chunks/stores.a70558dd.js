eb9e7c19cce4da9960d87a75d86c7d503d1fa887
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-12-11 11:46:01 +0100

    Updates

---
+++ _app/immutable/chunks/stores.a70558dd.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.1d524882.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
