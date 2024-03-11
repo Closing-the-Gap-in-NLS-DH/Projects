08e0d8b6eee8a77fc48583d5110a98e6e867d970
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-03-11 22:27:40 +0100

    Comment out unused code

--- src/routes/vis/datamanagement.ts
+++ src/routes/vis/datamanagement.ts
@@ -1,47 +1,47 @@
 import entriesRaw from '../../data/ENTRIES.json';
-import type { JsonStuff } from '$lib/utils.svelte';
+// import type { JsonStuff } from '$lib/utils.svelte';
 
-const projects = [
-	{
-		id: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a',
-		name: 'projects'
-	},
-	{
-		id: 'f472c52a-6da4-4392-9f8c-464fde1ef0f2',
-		name: 'research',
-		parent: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a'
-	},
-	{
-		id: 'e62091d9-1840-49aa-ba52-2fa18374e4fd',
-		name: 'infra',
-		parent: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a'
-	},
-	{
-		id: '125eeb61-6a53-4ece-9a5f-a94c6be92ea9',
-		name: 'other',
-		parent: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a'
-	},
-	{
-		id: 'de7d562f-b513-4b00-a64d-78f9c60d77d0',
-		name: 'Digital Preservation',
-		parent: 'f472c52a-6da4-4392-9f8c-464fde1ef0f2'
-	},
-	{
-		id: 'd17170ad-a131-4478-9d00-75231fd9a8cd',
-		name: 'Tools and Analysis',
-		parent: 'f472c52a-6da4-4392-9f8c-464fde1ef0f2'
-	},
-	{
-		id: 'dc646dac-666b-4734-9dbf-409576635a4a',
-		name: 'Infrastructure',
-		parent: 'e62091d9-1840-49aa-ba52-2fa18374e4fd'
-	},
-	{
-		id: '90886ac6-e53d-417a-960c-688a530cd4ea',
-		name: 'Other',
-		parent: '125eeb61-6a53-4ece-9a5f-a94c6be92ea9'
-	}
-];
+// const projects = [
+// 	{
+// 		id: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a',
+// 		name: 'projects'
+// 	},
+// 	{
+// 		id: 'f472c52a-6da4-4392-9f8c-464fde1ef0f2',
+// 		name: 'research',
+// 		parent: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a'
+// 	},
+// 	{
+// 		id: 'e62091d9-1840-49aa-ba52-2fa18374e4fd',
+// 		name: 'infra',
+// 		parent: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a'
+// 	},
+// 	{
+// 		id: '125eeb61-6a53-4ece-9a5f-a94c6be92ea9',
+// 		name: 'other',
+// 		parent: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a'
+// 	},
+// 	{
+// 		id: 'de7d562f-b513-4b00-a64d-78f9c60d77d0',
+// 		name: 'Digital Preservation',
+// 		parent: 'f472c52a-6da4-4392-9f8c-464fde1ef0f2'
+// 	},
+// 	{
+// 		id: 'd17170ad-a131-4478-9d00-75231fd9a8cd',
+// 		name: 'Tools and Analysis',
+// 		parent: 'f472c52a-6da4-4392-9f8c-464fde1ef0f2'
+// 	},
+// 	{
+// 		id: 'dc646dac-666b-4734-9dbf-409576635a4a',
+// 		name: 'Infrastructure',
+// 		parent: 'e62091d9-1840-49aa-ba52-2fa18374e4fd'
+// 	},
+// 	{
+// 		id: '90886ac6-e53d-417a-960c-688a530cd4ea',
+// 		name: 'Other',
+// 		parent: '125eeb61-6a53-4ece-9a5f-a94c6be92ea9'
+// 	}
+// ];
 
 for (let i = 0; i < entriesRaw.length; i++) {
 	console.log(entriesRaw[i]);
