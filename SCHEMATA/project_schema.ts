64ef9306b8c94ffeed1039ba704b448c4f2e3591
Kudela <kudem94@fu-berlin.de>
2024-03-12 13:21:51 +0100

    update the schema

--- SCHEMATA/project_schema.ts
+++ SCHEMATA/project_schema.ts
@@ -354,6 +354,9 @@ export const projectSchema = z
               ),
           )
           .describe("List of keywords to describe the project"),
+        category: z
+          .enum(["digital_preservation", "tools_and_analysis", "infrastructure", "other"])
+          .describe("Research focus ( digital_preservation | tools_and_analysis | infrastructure | other)"),
         comment: z
           .string()
           .describe("Any commentary that doesn't fit elsewhere in the schema"),
