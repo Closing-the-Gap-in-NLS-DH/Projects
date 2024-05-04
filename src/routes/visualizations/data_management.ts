e837093fc3ab39f0c7a6adf7f56bfdff900f95e3
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2024-05-04 11:24:13 +0200

    Refactor data management script

--- src/routes/visualizations/data_management.ts
+++ src/routes/visualizations/data_management.ts
@@ -1,18 +1,19 @@
 import entriesRaw from '../../data/ENTRIES.json';
 import type { JsonStuff } from '$lib/utils.svelte';
 
-export function createData() {
-	interface Dependency {
-		source: string;
-		target: string;
-	}
+interface Dependency {
+	source: string;
+	target: string;
+}
 
+export function createData() {
 	const entries = entriesRaw as [string, JsonStuff][];
-	const siblingDependencies: Dependency[] = [];
+
 	const parentChildDependencies: Dependency[] = [];
+	const siblingDependencies: Dependency[] = [];
 	const cooperationDependencies: Dependency[] = [];
 
-	const projects = [
+	const projects: Record<string, string>[] = [
 		{
 			id: 'd05b72be-a5aa-40e4-9458-cd39a739ee5a',
 			name: 'projects'
@@ -56,51 +57,75 @@ export function createData() {
 
 	for (const row of entries) {
 		const uuid = row[1]['record_metadata']['uuid'];
-		const relations = row[1]['project']['related_entities'];
+		const relations: Record<string, string>[] = row[1]['project']['related_entities'];
 
-		//create projects array
-		function appendProjects(id: string, parentname: string, uuid: string) {
-			let name;
-			if (row[1]['project']['title'].length <= 50) {
-				name = row[1]['project']['title'];
-			} else {
-				name = row[1]['project']['title'].slice(0, 50) + '...';
-			}
-			if (row[1]['project']['category'] == parentname) {
-				const projectItem = {
-					id: uuid,
-					name: name,
-					parent: id,
-					parentname: parentname,
-					url: row[1]['project']['websites'][0]
-				};
+		// Create projects array
+		appendProjects(projects, row, '90886ac6-e53d-417a-960c-688a530cd4ea', 'other', uuid);
+		appendProjects(
+			projects,
+			row,
+			'de7d562f-b513-4b00-a64d-78f9c60d77d0',
+			'digital_preservation',
+			uuid
+		);
+		appendProjects(
+			projects,
+			row,
+			'd17170ad-a131-4478-9d00-75231fd9a8cd',
+			'tools_and_analysis',
+			uuid
+		);
+		appendProjects(projects, row, 'dc646dac-666b-4734-9dbf-409576635a4a', 'infrastructure', uuid);
 
-				projects.push(projectItem);
-			}
-		}
-		appendProjects('90886ac6-e53d-417a-960c-688a530cd4ea', 'other', uuid);
-		appendProjects('de7d562f-b513-4b00-a64d-78f9c60d77d0', 'digital_preservation', uuid);
-		appendProjects('d17170ad-a131-4478-9d00-75231fd9a8cd', 'tools_and_analysis', uuid);
-		appendProjects('dc646dac-666b-4734-9dbf-409576635a4a', 'infrastructure', uuid);
-
-		//create dependencies
-		function appendDependencies(relationType: string, dependencyType: Dependency[], uuid: string) {
-			for (const row of relations) {
-				if (row['relation_type'] == relationType) {
-					const dependencyItem = {
-						source: uuid,
-						target: row['uuid']
-					};
-					//console.log(dependencyItem)
-					dependencyType.push(dependencyItem);
-				}
-			}
-		}
-
-		appendDependencies('sibling', siblingDependencies, uuid);
-		appendDependencies('child', parentChildDependencies, uuid);
-		appendDependencies('cooperation', cooperationDependencies, uuid);
+		// Create dependencies
+		appendDependencies(relations, 'sibling', siblingDependencies, uuid);
+		appendDependencies(relations, 'child', parentChildDependencies, uuid);
+		appendDependencies(relations, 'cooperation', cooperationDependencies, uuid);
 	}
 
 	return { projects, siblingDependencies, parentChildDependencies, cooperationDependencies };
+}
+
+function appendProjects(
+	projects: Record<string, string>[],
+	row: [string, JsonStuff],
+	parentId: string,
+	parentName: string,
+	id: string
+) {
+	let name = row[1]['project']['title'];
+
+	if (name.length > 50) {
+		name = name.slice(0, 50) + '…';
+	}
+
+	if (row[1]['project']['category'] === parentName) {
+		const projectItem = {
+			id,
+			name,
+			parent: parentId,
+			parentname: parentName,
+			url: row[1]['project']['websites'][0]
+		};
+
+		projects.push(projectItem);
+	}
+}
+
+function appendDependencies(
+	relations: Record<string, string>[],
+	relationType: string,
+	dependencyType: Dependency[],
+	uuid: string
+) {
+	for (const row of relations) {
+		if (row['relation_type'] === relationType) {
+			const dependencyItem = {
+				source: uuid,
+				target: row['uuid']
+			};
+
+			dependencyType.push(dependencyItem);
+		}
+	}
 }
